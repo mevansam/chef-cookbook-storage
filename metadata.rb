ebd1a8d5d7958430954264f7d7abfef933929265
Mevan Samaratunga <mevan.samaratunga@fmr.com>
2014-08-08 12:22:17 -0400

    Initial Commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'storage'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures storage'
+long_description 'Installs/Configures storage'
+version          '0.1.0'
+
